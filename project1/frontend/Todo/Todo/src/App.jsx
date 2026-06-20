30168df686e16e820f9eff9d712f8fc1bf7fe5db
Daneshwar <saiguptha663@gmail.com>
2026-06-21 00:07:35 +0530

    small chages in App.css and App.jsx

--- project1/frontend/Todo/Todo/src/App.jsx
+++ project1/frontend/Todo/Todo/src/App.jsx
@@ -1,34 +1,51 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import axios from "axios"
-import { useState } from 'react';
-function App(){
-  const [userName, setUserName] = useState();
-  const [password, setPassword] = useState();
-   const callApi = async()=>{
-        try{
-           const Data =await axios.get("http://localhost:8000")
-           console.log(Data)
-        }
-        catch(error){
-           console.log("Error was detected", error);
-        }
-   }
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+function App() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const callApi = async () => {
+    try {
+      const Data = await axios.get("http://localhost:8000");
+      console.log(Data);
+    } catch (error) {
+      console.log("Error was detected", error);
+    }
+  };
 
-  return(
-    <div>
-      <h1>Login Page</h1>
-      <div>
-        <label htmlFor="userName">Name</label>
-        <br/>
-        <input type="text" id="userName" placeholder="password" className='input' onChange={((e)=>setUserName(event.target.value))}/>
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1 className="login-title">Login Page</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            id="userName"
+            placeholder=" "
+            required
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <label htmlFor="userName">Username</label>
+        </div>
+
+        <div className="input-field">
+          <input
+            type="password"
+            id="password"
+            placeholder=" "
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+        <button className="btn btn-primary login-button" onClick={callApi}>
+          Click
+        </button>
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <br/>
-        <input type="password"id="password" placeholder="password" onChange={(e)=>setPassword(e.target.value)}/>
-      </div>
-      <button className='btn btn-primary' onClick={callApi}>Click</button>
     </div>
-  )
+  );
 }
-export default App
+export default App;
